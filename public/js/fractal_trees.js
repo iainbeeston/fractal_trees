ecb4eb3796fb9c506b8e87b5fd827824f0ce5d18
Iain Beeston <iain.beeston@gmail.com>
2014-10-31 11:51:49 +0000

    Made functions set to variables (rather than scope-local globals)

--- public/js/fractal_trees.js
+++ public/js/fractal_trees.js
@@ -1,8 +1,5 @@
 (function() {
-  var canvas = document.getElementById('canvas');
-  var ctx = canvas.getContext('2d');
-
-  function setStyles(styles) {
+  var setStyles = function (styles) {
     style = styles || {};
     for (var s in styles) {
       if (styles.hasOwnProperty(s)) {
@@ -11,21 +8,21 @@
     }
   }
 
-  function branchLength() {
+  var branchLength = function() {
     return chance.normal({mean: 8.0, dev: 1.0});
   }
 
-  function branchAngle() {
+  var branchAngle = function() {
     return chance.normal({mean: Math.PI / 9.0, dev: Math.PI / 18.0});
   }
 
-  function drawLine(x1, y1, x2, y2, styles){
+  var drawLine = function(x1, y1, x2, y2, styles){
     setStyles(styles);
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
   }
 
-  function drawTree(x1, y1, angle1, depth, styles){
+  var drawTree = function(x1, y1, angle1, depth, styles){
     if (depth === 0) return;
     var x2 = x1 + (Math.cos(angle1) * depth * branchLength());
     var y2 = y1 + (Math.sin(angle1) * depth * branchLength());
@@ -34,6 +31,8 @@
     drawTree(x2, y2, angle1 + branchAngle(), depth - 1, styles);
   }
 
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
   ctx.beginPath();
   drawTree(canvas.clientWidth/2, canvas.clientHeight, -Math.PI/2, 9, {strokeStyle: '#aa0000', lineWidth: 1});
   ctx.closePath();
