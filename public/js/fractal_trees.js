738deb8083b6af908a0879c5c5aa8d1c57d7a151
Iain Beeston <iain.beeston@gmail.com>
2014-10-31 11:43:47 +0000

    Made the branch length a variable

--- public/js/fractal_trees.js
+++ public/js/fractal_trees.js
@@ -2,6 +2,8 @@
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
 
+  var depth = 9;
+  var branchLength = 8.0;
   var angleMean = Math.PI / 9;
   var angleStDev = Math.PI / 18;
 
@@ -18,15 +20,15 @@
 
   function drawTree(x1, y1, angle1, depth, styles){
     if (depth === 0) return;
-    var x2 = x1 + (Math.cos(angle1) * depth * 10.0);
-    var y2 = y1 + (Math.sin(angle1) * depth * 10.0);
+    var x2 = x1 + (Math.cos(angle1) * depth * branchLength);
+    var y2 = y1 + (Math.sin(angle1) * depth * branchLength);
     drawLine(x1, y1, x2, y2, styles);
     drawTree(x2, y2, angle1 - chance.normal({mean: angleMean, dev: angleStDev}), depth - 1, styles);
     drawTree(x2, y2, angle1 + chance.normal({mean: angleMean, dev: angleStDev}), depth - 1, styles);
   }
 
   ctx.beginPath();
-  drawTree(canvas.clientWidth/2, canvas.clientHeight, -Math.PI/2, 9, {strokeStyle: '#aa0000', lineWidth: 1});
+  drawTree(canvas.clientWidth/2, canvas.clientHeight, -Math.PI/2, depth, {strokeStyle: '#aa0000', lineWidth: 1});
   ctx.closePath();
   ctx.stroke();
 })();
