95bbf41b54567b7f0c86d9ebadc38f22468a4cde
Iain Beeston <iain.beeston@gmail.com>
2014-10-31 11:46:27 +0000

    Made the branch lengths random

--- public/js/fractal_trees.js
+++ public/js/fractal_trees.js
@@ -3,9 +3,11 @@
   var ctx = canvas.getContext('2d');
 
   var depth = 9;
-  var branchLength = 8.0;
-  var angleMean = Math.PI / 9;
-  var angleStDev = Math.PI / 18;
+  var branchMean = 8.0;
+  var branchStDev = 1.0;
+  var branchLength = 7.5;
+  var angleMean = Math.PI / 9.0;
+  var angleStDev = Math.PI / 18.0;
 
   function drawLine(x1, y1, x2, y2, styles){
     style = styles || {};
@@ -20,8 +22,8 @@
 
   function drawTree(x1, y1, angle1, depth, styles){
     if (depth === 0) return;
-    var x2 = x1 + (Math.cos(angle1) * depth * branchLength);
-    var y2 = y1 + (Math.sin(angle1) * depth * branchLength);
+    var x2 = x1 + (Math.cos(angle1) * depth * chance.normal({mean: branchMean, dev: branchStDev}));
+    var y2 = y1 + (Math.sin(angle1) * depth * chance.normal({mean: branchMean, dev: branchStDev}));
     drawLine(x1, y1, x2, y2, styles);
     drawTree(x2, y2, angle1 - chance.normal({mean: angleMean, dev: angleStDev}), depth - 1, styles);
     drawTree(x2, y2, angle1 + chance.normal({mean: angleMean, dev: angleStDev}), depth - 1, styles);
