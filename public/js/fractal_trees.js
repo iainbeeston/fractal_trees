4d90e8f2b6700c2e24f2bcad9b76dc06e2299d07
Iain Beeston <iain.beeston@gmail.com>
2014-10-31 11:50:54 +0000

    Extracted branch random variables out to functions

--- public/js/fractal_trees.js
+++ public/js/fractal_trees.js
@@ -2,35 +2,40 @@
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
 
-  var depth = 9;
-  var branchMean = 8.0;
-  var branchStDev = 1.0;
-  var branchLength = 7.5;
-  var angleMean = Math.PI / 9.0;
-  var angleStDev = Math.PI / 18.0;
-
-  function drawLine(x1, y1, x2, y2, styles){
+  function setStyles(styles) {
     style = styles || {};
     for (var s in styles) {
       if (styles.hasOwnProperty(s)) {
         ctx[s] = styles[s];
       }
     }
+  }
+
+  function branchLength() {
+    return chance.normal({mean: 8.0, dev: 1.0});
+  }
+
+  function branchAngle() {
+    return chance.normal({mean: Math.PI / 9.0, dev: Math.PI / 18.0});
+  }
+
+  function drawLine(x1, y1, x2, y2, styles){
+    setStyles(styles);
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
   }
 
   function drawTree(x1, y1, angle1, depth, styles){
     if (depth === 0) return;
-    var x2 = x1 + (Math.cos(angle1) * depth * chance.normal({mean: branchMean, dev: branchStDev}));
-    var y2 = y1 + (Math.sin(angle1) * depth * chance.normal({mean: branchMean, dev: branchStDev}));
+    var x2 = x1 + (Math.cos(angle1) * depth * branchLength());
+    var y2 = y1 + (Math.sin(angle1) * depth * branchLength());
     drawLine(x1, y1, x2, y2, styles);
-    drawTree(x2, y2, angle1 - chance.normal({mean: angleMean, dev: angleStDev}), depth - 1, styles);
-    drawTree(x2, y2, angle1 + chance.normal({mean: angleMean, dev: angleStDev}), depth - 1, styles);
+    drawTree(x2, y2, angle1 - branchAngle(), depth - 1, styles);
+    drawTree(x2, y2, angle1 + branchAngle(), depth - 1, styles);
   }
 
   ctx.beginPath();
-  drawTree(canvas.clientWidth/2, canvas.clientHeight, -Math.PI/2, depth, {strokeStyle: '#aa0000', lineWidth: 1});
+  drawTree(canvas.clientWidth/2, canvas.clientHeight, -Math.PI/2, 9, {strokeStyle: '#aa0000', lineWidth: 1});
   ctx.closePath();
   ctx.stroke();
 })();
