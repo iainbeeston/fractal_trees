d61611025434e6bf4dea9f18a11cd4a9fdf81a0d
Iain Beeston <iain.beeston@gmail.com>
2014-10-31 12:02:36 +0000

    Made branches get thinner as they get further from the root

--- public/js/fractal_trees.js
+++ public/js/fractal_trees.js
@@ -8,33 +8,37 @@
     }
   }
 
-  var branchLength = function() {
-    return chance.normal({mean: 8.0, dev: 1.0});
+  var branchThickness = function(depth) {
+    return 1.5 * depth;
   }
 
-  var branchAngle = function() {
+  var branchLength = function(depth) {
+    return chance.normal({mean: 8.0, dev: Math.sqrt(depth)});
+  }
+
+  var branchAngle = function(depth) {
     return chance.normal({mean: Math.PI / 9.0, dev: Math.PI / 18.0});
   }
 
   var drawLine = function(x1, y1, x2, y2, styles){
-    setStyles(styles);
+    ctx.beginPath();
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
+    ctx.closePath()
+    ctx.stroke()
   }
 
   var drawTree = function(x1, y1, angle1, depth, styles){
     if (depth === 0) return;
-    var x2 = x1 + (Math.cos(angle1) * depth * branchLength());
-    var y2 = y1 + (Math.sin(angle1) * depth * branchLength());
+    var x2 = x1 + (Math.cos(angle1) * depth * branchLength(depth));
+    var y2 = y1 + (Math.sin(angle1) * depth * branchLength(depth));
+    setStyles({lineWidth: branchThickness(depth), strokeStyle: '#aa0000'});
     drawLine(x1, y1, x2, y2, styles);
-    drawTree(x2, y2, angle1 - branchAngle(), depth - 1, styles);
-    drawTree(x2, y2, angle1 + branchAngle(), depth - 1, styles);
+    drawTree(x2, y2, angle1 - branchAngle(depth), depth - 1, styles);
+    drawTree(x2, y2, angle1 + branchAngle(depth), depth - 1, styles);
   }
 
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
-  ctx.beginPath();
-  drawTree(canvas.clientWidth/2, canvas.clientHeight, -Math.PI/2, 9, {strokeStyle: '#aa0000', lineWidth: 1});
-  ctx.closePath();
-  ctx.stroke();
+  drawTree(canvas.clientWidth/2, canvas.clientHeight, -Math.PI/2, 9);
 })();
