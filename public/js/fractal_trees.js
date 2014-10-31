899d2ac901a385cbaf941e1ce61251380295311d
Iain Beeston <iain.beeston@gmail.com>
2014-10-31 14:01:08 +0000

    Made drawLine() into drawBranch()
    Seems to make more sense

--- public/js/fractal_trees.js
+++ public/js/fractal_trees.js
@@ -14,7 +14,7 @@
   }
 
   var branchLength = function(depth) {
-    return chance.normal({mean: 8.0, dev: 3.0});
+    return depth * chance.normal({mean: 8.0, dev: 3.0});
   }
 
   var branchAngle = function(depth) {
@@ -25,7 +25,7 @@
     return Math.floor(chance.normal({mean: 9.0, dev: 1.0}));
   }
 
-  var drawLine = function(x1, y1, x2, y2, styles){
+  var drawBranch = function(x1, y1, x2, y2, styles){
     Utils.extend(ctx, styles);
     ctx.beginPath();
     ctx.moveTo(x1, y1);
@@ -36,10 +36,10 @@
 
   var drawTree = function(x1, y1, angle1, depth, styles){
     if (depth === 0) return;
-    var x2 = x1 + (Math.cos(angle1) * depth * branchLength(depth));
-    var y2 = y1 + (Math.sin(angle1) * depth * branchLength(depth));
+    var x2 = x1 + (Math.cos(angle1) * branchLength(depth));
+    var y2 = y1 + (Math.sin(angle1) * branchLength(depth));
     styles = Utils.merge(styles, {lineWidth: branchThickness(depth)});
-    drawLine(x1, y1, x2, y2, styles);
+    drawBranch(x1, y1, x2, y2, styles);
     drawTree(x2, y2, angle1 - branchAngle(depth), depth - 1, styles);
     drawTree(x2, y2, angle1 + branchAngle(depth), depth - 1, styles);
   }
