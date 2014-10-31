86f0560d31d1c499971feb63e4f223c6d7370ba1
Iain Beeston <iain.beeston@gmail.com>
2014-10-31 17:10:09 +0000

    Split drawTree into a couple of subroutines, for the trunk and for the leaves

--- public/js/fractal_trees.js
+++ public/js/fractal_trees.js
@@ -76,59 +76,67 @@
     ctx.fill();
   }
 
+  var drawTrunk = function(x1, y1, angle1, depth, styles){
+    styles = Utils.merge(styles, {lineWidth: branchThickness(depth)});
+    var length = branchLength(depth);
+    var startTime = null;
+    var growTime = branchGrowTime(depth);
+
+    var cosAngle = Math.cos(angle1)
+    var sinAngle = Math.sin(angle1)
+    var x2 = x1;
+    var y2 = y2;
+
+    var step = function(timestamp) {
+      if (startTime === null) startTime = timestamp;
+      var currentTime = (timestamp - startTime);
+      var currentLength = length * (currentTime / growTime);
+
+      if (currentLength < length) {
+        x2 = x1 + (cosAngle * currentLength);
+        y2 = y1 + (sinAngle * currentLength);
+        drawBranch(x1, y1, x2, y2, styles);
+        window.requestAnimationFrame(step);
+      } else {
+        drawTree(x2, y2, angle1 - branchAngle(depth), depth - 1, styles);
+        drawTree(x2, y2, angle1 + branchAngle(depth), depth - 1, styles);
+      }
+    }
+
+    window.requestAnimationFrame(step);
+  }
+
+  var drawLeaves = function(x1, y1, angle1, depth, styles){
+    styles = Utils.merge(styles, {fillStyle: leafColour()});
+
+    var leafCount = leavesPerBranch();
+    for(var i = 0; i < leafCount; i++) {
+      var angle2 = leafAngle();
+      var currentLeafAngle = angle1 + angle2;
+      var length = leafLength();
+
+      var x2 = x1 + (Math.cos(currentLeafAngle) * length);
+      var y2 = y1 + (Math.sin(currentLeafAngle) * length);
+
+      var angle3 = currentLeafAngle + (Math.PI / 4.0);
+      var x3 = x1 + (Math.cos(angle3) * length / 3.0);
+      var y3 = y1 + (Math.sin(angle3) * length / 3.0);
+
+      var angle4 = currentLeafAngle - (Math.PI / 4.0);
+      var x4 = x1 + (Math.cos(angle4) * length / 3.0);
+      var y4 = y1 + (Math.sin(angle4) * length / 3.0);
+
+      setTimeout(function() {
+        drawLeaf(x1, y1, x2, y2, x3, y3, x4, y4, styles)
+      }, leafGrowTime());
+    }
+  }
+
   var drawTree = function(x1, y1, angle1, depth, styles){
     if (depth === 0) {
-      styles = Utils.merge(styles, {fillStyle: leafColour()});
-
-      var leafCount = leavesPerBranch();
-      for(var i = 0; i < leafCount; i++) {
-        var angle2 = leafAngle();
-        var currentLeafAngle = angle1 + angle2;
-        var length = leafLength();
-
-        var x2 = x1 + (Math.cos(currentLeafAngle) * length);
-        var y2 = y1 + (Math.sin(currentLeafAngle) * length);
-
-        var angle3 = currentLeafAngle + (Math.PI / 4.0);
-        var x3 = x1 + (Math.cos(angle3) * length / 3.0);
-        var y3 = y1 + (Math.sin(angle3) * length / 3.0);
-
-        var angle4 = currentLeafAngle - (Math.PI / 4.0);
-        var x4 = x1 + (Math.cos(angle4) * length / 3.0);
-        var y4 = y1 + (Math.sin(angle4) * length / 3.0);
-
-        setTimeout(function() {
-          drawLeaf(x1, y1, x2, y2, x3, y3, x4, y4, styles)
-        }, leafGrowTime());
-      }
+      drawLeaves(x1, y1, angle1, depth, styles)
     } else {
-      styles = Utils.merge(styles, {lineWidth: branchThickness(depth)});
-      var length = branchLength(depth);
-      var startTime = null;
-      var growTime = branchGrowTime(depth);
-
-      var cosAngle = Math.cos(angle1)
-      var sinAngle = Math.sin(angle1)
-      var x2 = x1;
-      var y2 = y2;
-
-      var step = function(timestamp) {
-        if (startTime === null) startTime = timestamp;
-        var currentTime = (timestamp - startTime);
-        var currentLength = length * (currentTime / growTime);
-
-        if (currentLength < length) {
-          x2 = x1 + (cosAngle * currentLength);
-          y2 = y1 + (sinAngle * currentLength);
-          drawBranch(x1, y1, x2, y2, styles);
-          window.requestAnimationFrame(step);
-        } else {
-          drawTree(x2, y2, angle1 - branchAngle(depth), depth - 1, styles);
-          drawTree(x2, y2, angle1 + branchAngle(depth), depth - 1, styles);
-        }
-      }
-
-      window.requestAnimationFrame(step);
+      drawTrunk(x1, y1, angle1, depth, styles)
     }
   }
 
